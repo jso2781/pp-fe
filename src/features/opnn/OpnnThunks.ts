f564361612749a6fd4ecaafd117e2a2d87eb76b7
songjiwoong1020 <songjiwoong1020@gmail.com>
2026-01-13 18:58:35 +0900

    의견제안 thunk 수정

--- src/features/opnn/OpnnThunks.ts
+++ src/features/opnn/OpnnThunks.ts
@@ -6,11 +6,15 @@ import { mockOpnnList, OpnnPVO, OpnnRVO, OpnnListPVO, OpnnListRVO, OpnnDVO  } fr
 /**
  * 대국민포털_의견제안 입력 
  */
-export const insertOpnn = createAsyncThunk<number, OpnnPVO>(
+export const insertOpnn = createAsyncThunk<number, FormData>(
   '/opnn/insertOpnn',
-  async (params: OpnnPVO) => {
+  async (params: FormData) => {
     try {
-      const res = await https.post(insertOpnnApiPath(), params);
+      const res = await https.post(insertOpnnApiPath(), params, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
 
       const code = res.data?.code;
       //code값에 따라서 에러처리
