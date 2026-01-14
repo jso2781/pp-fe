203c89a60b7c63ae819b423ea63bb7b3df49359c
ryuryujie <ryuryujie@gmail.com>
2026-01-14 13:11:04 +0900

    div > Box  로 변경

--- src/screens/published/KIDS-PP-US-LG-05.tsx
+++ src/screens/published/KIDS-PP-US-LG-05.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Box, Typography } from '@mui/material';
 import PopupTemplate from '../templates/PopupTemplate'
 
 export default function KIDS_PP_US_LG_05() {
   const config = {
     content: (
-      <div className="modal-content">
-        <div className="modal-desc">
+      <Box className="modal-content">
+        <Box className="modal-desc">
           <p>비밀번호가 5회이상 잘못입력되어, 비밀번호를 재설정 후 이용할 수 있습니다. 비밀번호를 재설정하시겠습니까? </p>
-        </div>
-      </div>
+        </Box>
+      </Box>
     ),
     wrapClassName: 'modal-small',
     okText: "확인",
