f43c5ed0d7ffa5ff82fbba20d2817166b1c5b9dc
yikim <kyi7829@naver.com>
2026-01-14 17:46:24 +0900

    feat : [메인화면] 메인화면 컨텐츠 조회 REST 작업

---
+++ src/features/main/MainThunks.ts
@@ -0,0 +1,21 @@
+import https from '@/api/axiosInstance'
+import { selectMainContentsPath } from '@/api/main/MainApiPaths'
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import { MainRVO } from './MainTypes'
+
+/**
+ * 대국민포털_메인화면 컨텐츠 조회 
+ */
+export const selectMainContents = createAsyncThunk<MainRVO, void>(
+  '/api/main',
+  async () => {
+    try {
+      const res = await https.post(selectMainContentsPath(), null);
+      const payload = res.data?.data;
+      return payload;
+    }    
+    catch (e) {
+      console.log("Not found Main Contents");
+    }
+  }
+)
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -6,6 +6,8 @@ import pstReducer from '@/features/pst/PstSlice'
 import uiReducer from '@/features/ui/uiSlice'
 import faqReducer from '@/features/faq/FaqSlice'
 import opnnReducer from '@/features/opnn/OpnnSlice'
+import mainReducer from '@/features/main/MainSlice'
+
 
 const rootReducer = combineReducers({
   board: boardReducer,  // FIXME 테스트 이후 삭제예정
@@ -14,7 +16,8 @@ const rootReducer = combineReducers({
   menu: menuReducer,
   pst: pstReducer, // 게시판
   faq: faqReducer, // FAQ
-  opnn: opnnReducer // 의견제안
+  opnn: opnnReducer, // 의견제안
+  main: mainReducer // 메인화면
 })
 
 export default rootReducer
