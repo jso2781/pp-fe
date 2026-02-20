7216ccde39abe1d4cc552252d9233b78629e952c
ryuryujie <ryuryujie@gmail.com>
2026-02-20 16:24:53 +0900

    영문 메인, 레이아웃 작업중

--- src/components/common/EngFooter.tsx
+++ src/components/common/EngFooter.tsx
@@ -1,33 +1,19 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from "react";
 import { useTranslation } from 'react-i18next'
 import { Box, Link as MuiLink, Stack, Typography } from '@mui/material';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { Link, useLocation, useNavigate } from 'react-router-dom'
-import RelatedSites from './RelatedSites'
+import { Link } from 'react-router-dom'
 
 type TKey =
-  | 'cctvPolicy'
-  | 'rejectUnAuthorizedEmail'
-  | 'directions'
-  | 'privacyPolicy'
-  | 'termsOfUse'
-  | 'cctvPolicy'
   | 'youTube'
   | 'instagram'
   | 'blog'
   | 'kakao'
   | 'facebook'
   | 'sitemap'
+  | 'ContactUs'
  
-const FOOTER_INFO_LINKS: Array<{ key: string; labelKey: TKey; href: string }> = [
-  { key: 'sitemap', labelKey: 'sitemap', href: '#' },
-  { key: 'location', labelKey: 'directions', href: 'https://www.drugsafe.or.kr/iwt/ds/ko/introduction/EgovLocation.do' },
-]
-
 const FOOTER_LEGAL_LINKS: Array<{ key: string; labelKey: TKey; href: string; className?: string }> = [
-  { key: 'privacy', labelKey: 'privacyPolicy', href: '/ko/etc/PrivacyPolicy', className: 'point-link'},
-  { key: 'terms', labelKey: 'termsOfUse', href: '/ko/etc/Terms' },
-  { key: 'terms', labelKey: 'cctvPolicy', href: '/ko/etc/CctvPolicy' },
+  { key: 'terms', labelKey: 'ContactUs', href: '' },
 ]
 
 const FOOTER_SNS_LINKS: Array<{ key: string; labelKey: TKey; href: string }> = [
@@ -42,82 +28,38 @@ export default function EngFooter() {
   const { t } = useTranslation()
   const year = new Date().getFullYear()
 
-  
-  //퀵메뉴
-  const navigate = useNavigate();
-  const location = useLocation(); // 현재 경로 가져오기
-  
-  // 메인 페이지 판별: '/' 이거나 '/ko' 인 경우 모두 포함
-  const isMainPage = location.pathname === '/' || location.pathname === '/ko'
-
-  const [isVisible, setIsVisible] = useState(false)
-  // 타입스크립트 사용 시를 고려한 Ref 설정
-  const scrollTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null)
-
-  useEffect(() => {
-    const handleScroll = () => {
-      // 스크롤 시 메뉴 표시
-      setIsVisible(true)
-
-      // 이전 타이머가 있으면 취소
-      if (scrollTimeoutRef.current) {
-        clearTimeout(scrollTimeoutRef.current)
-      }
-
-      // 2초 뒤 숨김 처리
-      scrollTimeoutRef.current = setTimeout(() => {
-        setIsVisible(false)
-      }, 2000)
-    }
-
-    window.addEventListener('scroll', handleScroll)
-    
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-      if (scrollTimeoutRef.current) clearTimeout(scrollTimeoutRef.current)
-    }
-  }, [])
-
   return (
     
-
     <Box component="footer" className="footer">
-      {/* 퀵메뉴 */}
-      <Box 
-        component="aside" 
-        className={`quick-menu-wrap ${isVisible ? 'show' : 'hide'}`} 
-        aria-label="퀵 메뉴"
-      >
-        {isMainPage && (
-          <Box className="quick-item">
-            <button type="button" className="btn-quick open-popup">
-              <i className="ico-popup" aria-hidden="true" />
-            </button>
-            <span className="quick-txt">닫힌 팝업<br />다시 열기</span>
-          </Box>
-        )}
-
-        <Box className="quick-item">
-          <button type="button" className="btn-quick faq" onClick={() => navigate('/news/faq')}>
-            <i className="ico-faq" aria-hidden="true" />
-          </button>
-          <span className="quick-txt">자주 묻는<br />질문</span>
-        </Box>
-
-        <Box className="quick-item">
-          <button 
-            type="button" 
-            className="btn-quick top" 
-            onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-          >
-            <i className="ico-top" aria-hidden="true" />
-          </button>
-          <span className="quick-txt">위로가기</span>
-        </Box>
-      </Box>
-
-      <RelatedSites />
       <Box className="container">
+
+        {/* 정책 */}
+        <Box className="footer-meta-info">
+          <Box className="meta-links">
+            {FOOTER_LEGAL_LINKS.map((item) => (
+              <MuiLink
+                key={item.key}
+                href={item.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`meta-item ${item.className || ''}`}
+              >
+                {t(item.labelKey)}
+              </MuiLink>
+            ))}
+          </Box>
+        </Box>
+
+        {/* 주소정보 */}
+        <Box className="footer-corporate-info">
+          <Box className="info-txt-area">
+            <Box className="info-txt">
+              <Typography className="address">Korea Institute of Drug Safety & Risk Management</Typography>
+              <Typography>5th Fl., 30, Burim-ro 169beon-gil, Dongan-gu, Anyang-si, Gyeonggi-do, Republic of Korea</Typography>
+              <Typography>ADR call center   Tel. +82-2-2172-6700    Fax. +82-2-2172-6701</Typography>
+            </Box>
+          </Box>
+        </Box>
 
         {/* 로고, SNS */}
         <Box className="footer-brand">
@@ -148,60 +90,6 @@ export default function EngFooter() {
                 ))}
               </Stack>
             </Box>
-          </Box>
-        </Box>
-
-        {/* 주소정보, 하단 링크 */}
-        <Box className="footer-corporate-info">
-          <Box className="info-txt-area">
-            <Box className="info-txt">
-              <Typography className="address">{t("kidsAddress")}</Typography>
-              <Typography>{`${t("bizRegNo")} 101-82-21134`}</Typography>
-              <Typography>{`${t("mainTel")} 02-2172-6700`}</Typography>
-              <Typography>{`${t("fax")} 02-2172-6701`}</Typography>
-            </Box>
-          </Box>
-          
-          <Box className="info-link-area">
-            <Box className="info-link">
-              <Stack className="link-group">
-                {FOOTER_INFO_LINKS.map((item) => (
-                  <MuiLink
-                    key={item.key}
-                    href={item.href}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    underline="hover"
-                  >
-                    {t(item.labelKey)}
-                    <ChevronRightIcon className="link-icon" />
-                  </MuiLink>
-                ))}
-              </Stack>
-            </Box>
-          </Box>
-        </Box>
-
-        {/* 정책, 카피라이트 */}
-        <Box className="footer-meta-info">
-          <Box className="meta-links">
-            {FOOTER_LEGAL_LINKS.map((item) => (
-              <MuiLink
-                key={item.key}
-                href={item.href}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={`meta-item ${item.className || ''}`}
-              >
-                {t(item.labelKey)}
-              </MuiLink>
-            ))}
-          </Box>
-          
-          <Box className="copyright">
-            <Typography>
-              © Korea Institute of Drug Safety &amp; Risk Management. All rights reserved.
-            </Typography>
           </Box>
         </Box>
 
--- src/pages/en/Home.tsx
+++ src/pages/en/Home.tsx
@@ -1,8 +1,6 @@
-import { useMemo, useState, useRef, useEffect} from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Box, Button, Card, Grid, CardContent, Link, List, ListItem, Tab, Tabs, Typography, IconButton } from '@mui/material';
+import { useMemo, useState, useRef} from 'react'
+import { Box, IconButton,Typography } from '@mui/material';
 import { OpenInNew, PlayArrow, Pause } from '@mui/icons-material'
-import { useTranslation } from 'react-i18next';
 
 // Swiper
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -12,26 +10,8 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/grid';
-import { useAppDispatch, useAppSelector } from '@/store/hooks';
-import { selectMainContents } from '@/features/main/MainThunks';
-import type { PostVO } from '@/features/main/MainTypes';
-import { Link as RouterLink } from 'react-router-dom';
-
 
 export default function Home() {
-
- /*  const serviceShortcuts = useMemo(
-    () => [
-      { title: 'Domestic Adverse Event Reporting', url: 'https://kaers.drugsafe.or.kr' },
-      { title: 'International Adverse Event Reporting', url: 'https://www.drugsafe.or.kr' },
-      { title: 'Drug Adverse Reaction Relief Program', url: 'https://nedrug.mfds.go.kr' },
-      { title: 'Safety Information Disclosure', url: 'https://open.drugsafe.or.kr' },
-      { title: 'Medical Data Analysis Network (MOA)', url: 'https://moa.drugsafe.or.kr' },
-      { title: 'APEC Pharmacovigilance Training Program', url: 'https://kidscoe.drugsafe.or.kr' },
-      { title: 'Drug Safety Officer Training', url: 'https://pvtraining.drugsafe.or.kr' },
-    ],
-    [] */
-
   // ==========================================
   // 기본서비스
   // ==========================================
@@ -51,24 +31,35 @@ export default function Home() {
   const serviceShortcuts = useMemo(
     () => [
       { title: 'Domestic Adverse Event Reporting', url: 'https://kaers.drugsafe.or.kr/', iconUrl: '/img/shortcut_ico01.png' },
-      { title: '의약품부작용피해구제 민원신청', url: 'https://nedrug.mfds.go.kr/cntnts/230', iconUrl: '/img/shortcut_ico01.png' },
-      { title: '마약류 통합관리 시스템', url: 'https://www.nims.or.kr/', iconUrl: '/img/shortcut_ico01.png' },
-      { title: 'Drug Adverse Reaction Relief Program', url: 'https://nedrug.mfds.go.kr/index', iconUrl: '/img/shortcut_ico01.png' },
-      { title: '첨단바이오의약품 장기추적조사 시스템', url: 'https://ltfu.mfds.go.kr/main.do', iconUrl: '/img/shortcut_ico01.png' },
-      { title: 'Medical Data Analysis Network (MOA)', url: 'https://moa.drugsafe.or.kr/main;jsessionid=BD9ADAD3F45597B4C06571485AB61A8A', iconUrl: '/img/shortcut_ico01.png' },
-      { title: 'rug Safety Officer Training', url: 'https://pvtraining.drugsafe.or.kr/', iconUrl: '/img/shortcut_ico01.png' },
-      { title: 'Safety Information Disclosure', url: 'https://open.drugsafe.or.kr/', iconUrl: '/img/shortcut_ico01.png' },
-      { title: 'APEC Pharmacovigilance Training Program', url: 'https://kidscoe.drugsafe.or.kr/', iconUrl: '/img/shortcut_ico01.png' },
+      { title: '의약품부작용피해구제 민원신청', url: 'https://nedrug.mfds.go.kr/cntnts/230', iconUrl: '/img/shortcut_ico02.png' },
+      { title: '마약류 통합관리 시스템', url: 'https://www.nims.or.kr/', iconUrl: '/img/shortcut_ico03.png' },
+      { title: 'Drug Adverse Reaction Relief Program', url: 'https://nedrug.mfds.go.kr/index', iconUrl: '/img/shortcut_ico04.png' },
+      { title: '첨단바이오의약품 장기추적조사 시스템', url: 'https://ltfu.mfds.go.kr/main.do', iconUrl: '/img/shortcut_ico05.png' },
+      { title: 'Medical Data Analysis Network (MOA)', url: 'https://moa.drugsafe.or.kr/main;jsessionid=BD9ADAD3F45597B4C06571485AB61A8A', iconUrl: '/img/shortcut_ico06.png' },
+      { title: 'rug Safety Officer Training', url: 'https://pvtraining.drugsafe.or.kr/', iconUrl: '/img/shortcut_ico07.png' },
+      { title: 'Safety Information Disclosure', url: 'https://open.drugsafe.or.kr/', iconUrl: '/img/shortcut_ico08.png' },
+      { title: 'APEC Pharmacovigilance Training Program', url: 'https://kidscoe.drugsafe.or.kr/', iconUrl: '/img/shortcut_ico09.png' },
     ],
     []
   );
 
   return (
     <Box className="main-container">
-
+      <Box component="section" className="section main-visual-section">
+        <Box className="inner">
+          <Box className="slogan-group">
+            <Typography className="slogan-title">
+              The Korea Institute of Drug <br/>Safety and Risk Management
+            </Typography>
+            <Typography className="slogan-desc">
+              Manages drug safety issues systematically by <br/>evaluating drug risk and promoting safe drug use
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
       <Box component="section" className="section main-service-section">
         <Box className="inner">
-          <h3 className="section-title">Basic <span>Service</span></h3>
+          <h3 className="section-title">Basic Service</h3>
           <Box className="service-area">
             <Swiper
               onSwiper={(swiper) => (swiperRef2.current = swiper)}
@@ -92,7 +83,7 @@ export default function Home() {
                     <Box className="icon-bg" style={{ backgroundImage: `url(${s.iconUrl})` } as React.CSSProperties} aria-hidden="true" />
                     <span className="shortcut-text">{s.title}</span>
                     <Box component="span" className="shortcut-link-box">
-                      <span className="shortcut-link-text">바로가기</span>
+                      <span className="shortcut-link-text">Detail</span>
                       <OpenInNew className="shortcut-icon" />
                     </Box>
                   </a>
@@ -115,28 +106,6 @@ export default function Home() {
           </Box>
         </Box>
       </Box>
-      {/* <Box component="section" className="ds-section ds-fullbleed">
-        <Box className="ds-container">
-          <Box className="ds-section__head">
-            <Typography variant="h6" sx={{ m: 0 }}>
-              Quick Access to Services Provided by KIDS
-            </Typography>
-            <Typography variant="body2" sx={{ m: 0, color: 'var(--muted)' }}>
-              We introduce our key services.
-            </Typography>
-          </Box>
-
-          <Grid container spacing={1.5}>
-            {serviceShortcuts.map((s) => (
-              <Grid key={s.title} size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
-                <a className="ds-shortcut" href={s.url} target="_blank" rel="noreferrer">
-                  {s.title} <ChevronRight fontSize="small" />
-                </a>
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-      </Box> */}
     </Box>
   )
 }
