cc5a4223287dd6af42e30282a44c55d73fbb6102
ryuryujie <ryuryujie@gmail.com>
2026-02-20 14:04:05 +0900

    국문,영문 scss 분리

--- src/routes/EngLayout.tsx
+++ src/routes/EngLayout.tsx
@@ -16,14 +16,16 @@ export default function ExpertLayout() {
   return (
     <ThemeProvider theme={muiTheme}>
       <CssBaseline />
-      <EngHeader onOpenNav={() => setMobileOpen(true)} />
-      {/* <AppNavDrawer mobileOpen={mobileOpen} onCloseMobile={() => setMobileOpen(false)} drawerWidth={DRAWER_WIDTH} /> */}
-      <Box className="app-main" sx={{ flex: 1 }}>
-        <Suspense fallback={<div style={{ padding: 24, textAlign: 'center' }}>로딩 중...</div>}>
-          <Outlet />
-        </Suspense>
+      <Box className="lang-en">
+        <EngHeader onOpenNav={() => setMobileOpen(true)} />
+        {/* <AppNavDrawer mobileOpen={mobileOpen} onCloseMobile={() => setMobileOpen(false)} drawerWidth={DRAWER_WIDTH} /> */}
+        <Box className="app-main" sx={{ flex: 1 }}>
+          <Suspense fallback={<div style={{ padding: 24, textAlign: 'center' }}>로딩 중...</div>}>
+            <Outlet />
+          </Suspense>
+        </Box>
+        <EngFooter />
       </Box>
-      <EngFooter />
     </ThemeProvider>
   )
 }
