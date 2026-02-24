a115fc0dcd685f17dfe25197de3c3cd4a5c6c869
ryuryujie <ryuryujie@gmail.com>
2026-02-24 10:32:00 +0900

    lang-ko 클래스 추가

--- src/routes/BlankLayout.tsx
+++ src/routes/BlankLayout.tsx
@@ -1,5 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import { Box } from '@mui/material'
 
 export default function BlankLayout() {
-  return <Outlet />
+  return (
+    <Box className="lang-ko">
+      <Outlet />
+    </Box>
+  )
 }
