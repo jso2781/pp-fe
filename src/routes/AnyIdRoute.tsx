12e05bd4c0f40b08229197dc87c794a097ec5c6b
yiKim <yi7829@naver.com>
2026-03-24 17:12:08 +0900

    fix : [내업무] 인증 로직 수정

--- src/routes/AnyIdRoute.tsx
+++ src/routes/AnyIdRoute.tsx
@@ -16,8 +16,8 @@ function AnyIdRoute({ children }: AnyIdRouteProps) {
 
   const expertCertSuccess = sessionStorage.getItem('expertCertSuccess');
 
-  // FIXME 조건은 추후 로그인 구분코드로 변경
-  if (user?.userInfo?.mbrId === 'yi7829' && !expertCertSuccess) {
+  // ID/PW로 로그인 && 세션스토리지 인증이력 없을경우
+  if (user?.userInfo?.lgnSeCd === '1' && !expertCertSuccess) {
     return <Navigate to={`/pp/${i18n.language}/auth/ExpertCert`} replace />
   }
 
